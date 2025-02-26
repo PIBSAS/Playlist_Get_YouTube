7ef7a0d1a9c63d015f61bf13e6f14a8feb9266d9
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2025-02-26 11:05:51 -0300

    Update audio_yt_dlp.py

--- audio_yt_dlp.py
+++ audio_yt_dlp.py
@@ -2,6 +2,18 @@
 ## pip install yt-dlp ##
 ## sudo apt-get install ffmpeg ##
 
+import os, subprocess, sys
+
+def is_dependencies_installed():
+    try:
+        subprocess.check_output([sys.executable, "-m", "pip", "show", "yt_dlp"])
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+if not is_dependencies_installed():
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "yt_dlp"])
+
 import yt_dlp
 
 def descargar_audio(playlist_url):
